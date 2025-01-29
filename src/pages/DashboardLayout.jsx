b6f3b4383f2832d103215676d79557e32b055462
paulmcgonigle <S00210326@atu.ie>
2025-01-28 20:10:23 -0600

    added dashboard sideBar and bigSideBar

--- src/pages/DashboardLayout.jsx
+++ src/pages/DashboardLayout.jsx
@@ -1,4 +1,20 @@
+import { Outlet } from "react-router-dom";
+import Wrapper from "../assets/wrappers/Dashboard";
+import { BigSidebar, SmallSidebar, Navbar } from "../components";
 const DashboardLayout = () => {
-  return <h1>DashboardLayout</h1>;
+  return (
+    <Wrapper>
+      <main className="dashboard">
+        <SmallSidebar />
+        <BigSidebar />
+        <div>
+          <Navbar />
+          <div className="dashboard-page">
+            <Outlet />
+          </div>
+        </div>
+      </main>
+    </Wrapper>
+  );
 };
 export default DashboardLayout;
