fd748222165ec41d99e346d423c2d7905e3ae406
paulmcgonigle <S00210326@atu.ie>
2025-01-28 19:21:38 -0600

    initial setup with clientSide,Images etc

---
+++ src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+const Login = () => {
+  return (
+    <div>
+      <h1>Logins</h1>
+      <Link to="/register">Register Page</Link>
+    </div>
+  );
+};
+export default Login;
