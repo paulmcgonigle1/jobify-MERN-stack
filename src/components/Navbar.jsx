0d06c34dea146c2ff5d5ee7ea2da3e6cbce89aa8
paulmcgonigle <S00210326@atu.ie>
2025-01-29 12:15:11 -0600

    added FaIcons and Context

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,11 @@
 import Wrapper from "../assets/wrappers/Navbar";
-
+import { FaHome } from "react-icons/fa";
 const Navbar = () => {
-  return <Wrapper>Navbar</Wrapper>;
+  return (
+    <Wrapper>
+      <h2>navbar</h2>
+      <FaHome></FaHome>
+    </Wrapper>
+  );
 };
 export default Navbar;
